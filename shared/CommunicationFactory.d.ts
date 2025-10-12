b12165e744effdfa1bbd497750c61b8feae3ac8c
umairkhancis <umairkhancis@gmail.com>
2025-10-12 16:41:38 +0400

    clean up

--- shared/CommunicationFactory.d.ts
+++
@@ -1,8 +0,0 @@
-import { CommunicationConfig, ServerConfig } from './types';
-export declare class CommunicationFactory {
-    static createManager(config: CommunicationConfig): any;
-    static createServer(config: ServerConfig): any;
-    static validateClientConfig(config: CommunicationConfig): boolean;
-    static validateServerConfig(config: ServerConfig): boolean;
-}
-//# sourceMappingURL=CommunicationFactory.d.ts.map
